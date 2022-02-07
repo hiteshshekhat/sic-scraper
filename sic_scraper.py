ffd109f9099c88331bc66c7b8eaaf90cc1163a69
Hitesh Shekhat <shekhat.hitesh@gmail.com>
2022-02-07 09:16:15 +0530

    final commit

--- sic_scraper.py
+++ sic_scraper.py
@@ -1,11 +1,12 @@
 from unicodedata import category
 import requests
-import requests_cache
+# import requests_cache
 from time import time, sleep
 import random
 from bs4 import BeautifulSoup
+import json
 
-requests_cache.install_cache(expire_after=18000, allowable_methods=('GET', 'POST'))
+# requests_cache.install_cache(expire_after=18000, allowable_methods=('GET', 'POST'))
 
 
 with open("proxies_ip.txt", "r") as f:
@@ -34,10 +35,9 @@ def get_last_page_number(url):
     return total_page
 
 
+
+
 category_url = 'http://www.siclists.com/sic-code.html'
-
-
-
 r = requests.get(category_url)
 html_soup = BeautifulSoup(r.text, 'html.parser')
 category_li = html_soup.select('.statelistss ul li a')
@@ -45,29 +45,57 @@ category_urls = [a['href'] for a in category_li]
 category_dict = {a['href'] : a.get_text(strip=True) for a in category_li}
 print(category_urls)
 print(category_dict)
+print(len(category_urls))
 
 
-for url in category_urls[0:1]:
+
+for url in category_urls:
+    print()
+    print('###################')
+    print()
+    print(f'Extracting data for {category_dict[url]}')
+    print()
+    print('###################')
+    print()
     total_pages = get_last_page_number(url)
-    for i in range(1):
-        if i > 0:
-            cat_url = url[:-5] + '-' + str(random.randint(1, total_pages)) + url[-5:]
-        else:
+    print(f'{category_dict[url]} has {total_pages} pages')
+    print()
+    for i in range(50):
+        if i == 0:
             cat_url = url
-        print(cat_url)
+        elif i == 1:
+            cat_url = url[:-5] + '-' + str(total_pages) + url[-5:]
+        else:
+            cat_url = url[:-5] + '-' + str(random.randint(1, total_pages-1)) + url[-5:]
+        print(f'page no. {i} >>> {cat_url}')
+        print()
         r = send_request(cat_url, proxies)
         html_soup = BeautifulSoup(r.text, 'html.parser')
         a_tags = html_soup.select('.boxcon tr:nth-of-type(1) a')
         # print(a_tags)
-        for a in a_tags[0:1]:
+        for a in a_tags:
             company_url = a['href']
-            company_dict = {'seen_url':company_url, 'seen_epoch': time(), 'company_name':a.get_text(strip=True)}
-            print(company_dict)
+            company_name = a.get_text(strip=True)
+            print(f"Extracting data for {category_dict[url]}'s") 
+            print(f"page no. {i} >>> {cat_url}") 
+            print(f"Getting contact info for {company_name}")
+            company_dict = {'seen_url':company_url, 'seen_epoch': time(), 'company_name':company_name}
+            # print(company_dict)
             r = send_request(company_url, proxies)
             html_soup = BeautifulSoup(r.text, 'html.parser')
+            # info = html_soup.select('.infolist strong,.infolist span')
             info = html_soup.select('.infolist li')
-
-            print(info)
+            raw_address_text = [li.text for li in info][1:]
+            company_dict['contact_info'] = raw_address_text
+            # print(info)
+            # print(raw_address_text)
+            print(company_dict)
+            print()
+            print('###################')
+            print()
+            with open('company_info.jl', 'a') as f:
+                json.dump(company_dict, f)
+                f.write('\n')
 
 
 
